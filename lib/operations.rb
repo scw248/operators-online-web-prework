60f9693a96ec2ae4e942dfcec5ee6141bee79a7b
Stephen Wright <scw248@gmail.com>
2019-01-02 22:43:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -19,27 +19,4 @@ end
 
 
 
-  describe "#not_safe?" do
-    it 'returns true if speed is greater than 60' do
-      answer = not_safe?(79)
-      expect(answer).to eq(true)
-    end
-
-    it 'returns true if speed is less than 40' do
-      answer = not_safe?(35)
-      expect(answer).to eq(true)
-    end
-
-    it 'returns false if the speed is between 40 and 60' do
-      answer = not_safe?(50)
-      expect(answer).to eq(false)
-    end
-
-    it 'uses the ternary operator' do
-      methods = file_contents.split("not_safe?")
-      match = methods.last
-      expect(match).to include("?")
-    end
-  end
-  
-end
+ 
